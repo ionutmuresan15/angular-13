31c030495fa148b826355459edfd7aeaf121bb8f
ionutmuresan15 <plexaz.ionut@gmail.com>
2023-09-05 14:25:22 +0300

    directive-example - in progress

---
+++ directive-example/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HighlightDirective } from './highlight.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HighlightDirective
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ directive-example/src/app/highlight.directive.ts
@@ -0,0 +1,22 @@
+import { Directive, ElementRef, Renderer2, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appHighlight]'
+})
+export class HighlightDirective {
+
+  constructor(private element: ElementRef, private renderer: Renderer2) {
+
+   }
+
+   // checking if the condition from app.component.html is true
+   // (video with the most likes)
+   // if so then apply a css class on the html element
+   
+   @Input() set appHighlight(condition: boolean){
+    if(condition){
+      this.renderer.addClass(this.element.nativeElement, 'highlight')
+    }
+   }
+
+}
